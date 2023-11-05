a201530017794cac0c06bb506845c1d0389e64c3
Mateus de Carvalho <mateuscs3003@gmail.com>
2023-11-05 11:46:55 -0300

    fix: ajustando problema de CORS para consumir API no front-end

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { appDataSource } from "./data-source";
+import * as cors from 'cors';
 import routes from './routes';
+
+const options: cors.CorsOptions = {
+        methods: "GET, POST, PUT, DELETE",
+        origin: "*"
+    }
 
 appDataSource.initialize().then(() => {
     const app = express();
+
+    app.use(cors.default(options));
 
     app.use(express.json());
     
